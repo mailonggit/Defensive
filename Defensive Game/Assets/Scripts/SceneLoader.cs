b90233f593feaf981c8db121cb73b5b9c4733cbf
Mai Hoang Long <mailonggit@gmail.com>
2021-03-11 17:08:02 +0700

    create main menu scene

---
+++ Defensive Game/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(sceneName: "Level 2");
+    }
+    public void Option()
+    {
+
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
